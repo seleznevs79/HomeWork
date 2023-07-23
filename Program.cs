3ca8023e3b3b5ed6d32b7d745398ad6268532421
Serg <seleznevs79@mail.ru>
2023-07-23 15:35:25 +0700

    задача 1 исправления

--- Program.cs
+++ Program.cs
@@ -3,33 +3,35 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
-// Console.Write("Введите первое число: ");
-//  int first = int.Parse(Console.ReadLine()!);
-//  Console.Write("Введите второе число: ");
-//  int second = int.Parse(Console.ReadLine()!);
-//  if (first>second)
-//  {
-//  Console.WriteLine( first );
-// }
-//  else Console.WriteLine(second);
+Console.Write("Введите первое число: ");
+ int first = int.Parse(Console.ReadLine()!);
+ Console.Write("Введите второе число: ");
+ int second = int.Parse(Console.ReadLine()!);
+ if (first>second)
+ {
+ Console.Write ("Большее число =");   
+ Console.WriteLine( first );
+}
+ else
+  Console.WriteLine($"Большее число = {second}");
 
 //  Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 // 2, 3, 7 -> 7
 // 44 5 78 -> 78
 // 22 3 9 -> 22
 
-Console.Write("Введите первое число: ");
-int first = int.Parse(Console.ReadLine()!);
-Console.Write("Введите второе число: ");
-int second = int.Parse(Console.ReadLine()!);
-Console.Write("Введите третье число: ");
-int third = int.Parse(Console.ReadLine()!);
-int max= first;
+// Console.Write("Введите первое число: ");
+// int first = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите второе число: ");
+// int second = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите третье число: ");
+// int third = int.Parse(Console.ReadLine()!);
+// int max= first;
 
-if (max<first) max=first;
-if (max<second) max=second;
-if (max<third) max=third;
+// if (max<first) max=first;
+// if (max<second) max=second;
+// if (max<third) max=third;
 
-Console.Write ("max= ");
-Console.WriteLine (max);
+// Console.Write ("max= ");
+// Console.WriteLine (max);
 
